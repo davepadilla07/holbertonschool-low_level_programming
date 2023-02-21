0e7e4596099afe269a8de6928620b29b31358347
davepadilla07 <ing.dave@hotmail.es>
2023-02-21 12:52:17 -0800

    10. Printing from 0 to 99 followed by a comma and a space where the combination doesnt repeat.

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -10,26 +10,24 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 9; i++)
 	{
 
 		for (j = 1; i <= 9; j++)
 		{
-			if (i == j || i > j)
-			{
-				;
-			}
-			else
+			if (i != j)
 			{
 				putchar(i);
 				putchar(j);
-			}
 
-		}
-		if (i != 8 && j != 9)
-		{
-			putchar(',');
-			putchar(' ');
+				if (i == 8 && p == 9)
+				{
+					continue;
+				}
+
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
