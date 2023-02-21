44a2c6855597bbfe9e8e5701adaaf4cfd6bb6e77
davepadilla07 <ing.dave@hotmail.es>
2023-02-21 07:14:11 -0800

    4. Printing the alphabet without e nor q

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lettr;
+
+	for (lettr = 'a'; lettr <= 'z'; lettr++)
+	{
+
+		if (letter == 'e')
+
+			;
+
+		else if (lettr == 'q')
+
+			;
+
+		else
+			putchar(lettr);
+
+	}
+	putchar('\n');
+	return (0);
+}
--- variables_if_else_while/README.md
+++ variables_if_else_while/README.md
@@ -2,7 +2,7 @@
 # SCRIPT 1: Assigning a random number and showing its last number.
 # SCRIPT 2: Printing the alphabet in lowercase.
 # SCRIPT 3: Printing the alphabet in lowercase and uppercase.
-# SCRIPT 4: .
+# SCRIPT 4: Printing the alphabet without e nor q.
 # SCRIPT 5: .
 # SCRIPT 6: .
 # SCRIPT 7: .
