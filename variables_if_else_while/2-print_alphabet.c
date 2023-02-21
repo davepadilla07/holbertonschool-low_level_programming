49101f87d7279afc25dc7d9b2bb60225068ddd26
davepadilla07 <ing.dave@hotmail.es>
2023-02-21 06:51:28 -0800

    2. Printing the adphabet in lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lettr;
+
+	for (lettr = 'a'; lettr <= 'z'; lettr++)
+	{
+
+		putchar(letter);
+		putchat('\n');
+
+	}
+	return (0);
+}
--- variables_if_else_while/README.md
+++ variables_if_else_while/README.md
@@ -1,6 +1,6 @@
 # SCRIPT 0: Assigning a random number and showing whether is 0, positive or negative..
 # SCRIPT 1: Assigning a random number and showing its last number.
-# SCRIPT 2: .
+# SCRIPT 2: Printing the alphabet in lowercase.
 # SCRIPT 3: .
 # SCRIPT 4: .
 # SCRIPT 5: .
